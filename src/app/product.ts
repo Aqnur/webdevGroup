f1f395f21cfb58b3ca1a273906d8fc89119e23ef
Sultan <sultik.99.10@gmail.com>
2020-04-07 19:46:10 +0600

    Products added
    Product component added to the project

---
+++ src/app/product.ts
@@ -0,0 +1,12 @@
+export interface Product {
+    id: number;
+    categoryId:number;
+    name: string;
+    price: string;
+    description: string;
+    rating:any;
+    originalLink:string;
+    images:any;
+    categoryName:string;
+    view_count:number;
+  }
