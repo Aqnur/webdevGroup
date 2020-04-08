24e579826f9b2a14961c2b9cbfe96d6c5ed33a17
Sultan <sultik.99.10@gmail.com>
2020-04-08 16:01:09 +0600

    upd

---
+++ my-app/src/app/product.ts
@@ -0,0 +1,19 @@
+export interface Product {
+  id: number;
+  categoryId:number;
+  name: string;
+  price: string;
+  description: string;
+  rating:any;
+  originalLink:string;
+  images:any;
+  categoryName:string;
+  view_count:number;
+  screen1:string;
+  screen2:string;
+  screen3:string;
+  language: string;
+  release: string;
+  publisher: string;
+  developer: string;
+  }
